7f16f817037de4c370c517441d305dfb981eecf0
pulgarino222 <pulgarinhernandezjuanfelipe@gmail.com>
2024-07-05 17:20:09 -0500

    feat:ultima actualizacion

--- punto 2/src/js/main.js
+++ punto 2/src/js/main.js
@@ -1,26 +1,33 @@
 import "../scss/style.scss"
-import * as bootstrap from 'bootstrap'
 
-const url = 'http://localhost:3000/tareas'
+
+const url = 'http://localhost:3000/tareas';
 
 let form = document.querySelector("#form")
+let imageTask = document.querySelector("#image")
+let nameTask = document.querySelector("#nameTask")
+let descriptionTask = document.querySelector("#description")
+
 let main = document.querySelector("#main")
 
-
+btn.addEventListener("click", (e) => {
+  e.preventDefault()
+  console.log('hola');
+})
 
 
 let index = async (main, url) => {
   const data = await fetch(url)
-  const dataReel = await data.json()
-  for (let i = 0; i < dataReel.length; i++) {
+  const dataReal = await data.json()
+  for (let i = 0; i < dataReal.length; i++) {
     main.innerHTML += `
       
       <div class="card m-3" style="width: 18rem;">
-          <img src=${dataReel[i].image}
+          <img src=${dataReal[i].image}
               class="card-img-top" alt="...">
               <div class="card-body">
-              <h5 class="card-title">tarea: ${dataReel[i].nameTask}</h5>
-              <p>descripcion: ${dataReel[i].description} </p>
+              <h5 class="card-title">tarea: ${dataReal[i].nameTask}</h5>
+              <p>descripcion: ${dataReal[i].description} </p>
               <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
               <label class="form-check-label" for="flexCheckChecked">
@@ -28,8 +35,8 @@ let index = async (main, url) => {
               </label>
               </div>
               <div>
-                  <button type="button" class="btn btn-danger m-2" id="${dataReel[i].id}">delete</button>
-                  <button type="button" class="btn btn-warning" id="${dataReel[i].id}">edit</button>
+                  <button type="button" class="btn btn-danger m-2" id="${dataReal[i].id}">delete</button>
+                  <button type="button" class="btn btn-warning" id="${dataReal[i].id}">edit</button>
               </div>
           </div>
       </div> `
@@ -42,21 +49,23 @@ index(main, url)
 
 
 //this function inner a new information in the database 
-async function newData (brand,model,image) {
+async function newData(nameTask, descriptionTask, imageTask) {
   let newobject = {
-      brand: brand.value,
-      model: model.value,
-      image: image.value
+    nameTask: nameTask.value,
+    description: descriptionTask.value,
+    image: imageTask.value,
+    complete: false
+  }
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'content-Type': 'application/json'
+    },
+    body: JSON.stringify(newobject)
+  })
 }
-await fetch(url,{
-  method:'POST',
-  headers:{
-      'content-Type':'application/json'
-  },
-  body:JSON.stringify(newobject)
-})
-  location.reload()
-}
+
+
 
 
 
